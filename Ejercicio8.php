143fd9bbcd3d1060b1e6afd01d3ef4ae5d6c88d2
19863095-fxr <19863095@clases.edu.sv>
2026-05-20 10:47:04 -0600

    Ejercicio 8
    Cree un formulario que permita ingresar dos números y envíe la información a otro
    archivo PHP donde se indique cuál es el número menor.

---
+++ Ejercicio8.php
@@ -0,0 +1,31 @@
+<?php
+$num1 = $_POST['numero1'];
+$num2 = $_POST['numero2'];
+$resultado = "";
+
+if ($num1 < $num2) {
+    $resultado = "El primer numero (" . $num1 . ") es menor que el segundo numero (" . $num2 . ").";
+} elseif ($num2 < $num1) {
+    $resultado = "El segundo numero (" . $num2 . ") es menor que el primer numero (" . $num1 . ").";
+} else {
+    $resultado = "Ambos numeros son iguales (" . $num1 . ").";
+}
+
+echo "<body bgcolor='#cca9a9' text='#000000'>";
+
+echo "<h1>Resultado de la Comparacion</h1>";
+echo "Numero 1 ingresado: " . $num1;
+echo "<br>";
+echo "Numero 2 ingresado: " . $num2;
+echo "<br><br>";
+
+echo "<b>Respuesta: </b>" . $resultado;
+echo "<br><br>";
+echo "<hr>";
+
+echo "<a href='Ejercicio8.html'>Regresar al formulario</a>";
+echo "<br><br>";
+echo "<a href='index.html'>Regresar al menú principal</a>";
+
+echo "</body>";
+?>
